983770058ea737f012d8ca58bc54aa8b0909e564
Kee Cheol, David, Kim <38066609+kagented@users.noreply.github.com>
2020-07-17 21:27:38 +0900

    Update v2p.py

--- dictation/v2p.py
+++ dictation/v2p.py
@@ -8,25 +8,34 @@ r = sr.Recognizer()
 
 mic = sr.Microphone(device_index=1)
 # pprint.pprint(sr.Microphone.list_microphone_names())
-with mic as source:
-    print('say something')
-    audio = r.listen(source)
-    print('recording finished')
 
-recorded = r.recognize_google(audio, language="ko-KR")
+while True:
+    with mic as source:
+        print('찬양 제목을 말해주세요.')
+        audio = r.listen(source)
 
-text = recorded.replace(" ", "")
+    try:
+        recorded = r.recognize_google(audio, language="ko-KR")
 
-#불러온 제목 검색
+        text = recorded.replace(" ", "")
 
-dirname = r'C:\Users\USER\Desktop\예배곡ppt'
+        if text == '완료':
+            break
 
-dirlist = []
+        else:
+
+            #불러온 제목 검색
+
+            dirname = r'C:\Users\USER\Desktop\예배곡ppt'
+
+            dirlist = []
 
 
-for root, dirs, files in os.walk(dirname):
-    for file in files:
-        if file.startswith(text):
-            dirlist.append(os.path.join(root, file))
+            for root, dirs, files in os.walk(dirname):
+                for file in files:
+                    if file.startswith(text):
+                        dirlist.append(os.path.join(root, file))
 
-os.system('start "" "'+dirlist[0]+'""')
+            os.system('start "" "'+dirlist[0]+'""')
+    except:
+        print('잘 못 들었습니다.')
